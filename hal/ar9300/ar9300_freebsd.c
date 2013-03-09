961b17fd6f431f352b9472509f0f3d6f549584ff
Adrian Chadd <adrian@freebsd.org>
2013-03-09 11:15:57 -0800

    Fix copyright - this is actually my code.

--- hal/ar9300/ar9300_freebsd.c
+++ hal/ar9300/ar9300_freebsd.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2010, Atheros Communications Inc.
+ * Copyright (c) 2012, 2013 Adrian Chadd <adrian@FreeBSD.org>.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
